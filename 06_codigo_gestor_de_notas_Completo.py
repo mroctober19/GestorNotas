ea0e2a1403517c1bd64b7ee06075acb3ffc9ff60
mroctober19 <respinozaj@miumg.edu.gt>
2025-10-24 07:51:35 -0600

    06_codigo_gestor_de_notas_Completo.py

--- 06_codigo_gestor_de_notas_Completo.py
+++ 06_codigo_gestor_de_notas_Completo.py
@@ -1,20 +1,29 @@
-# CODIGO FINALIZADO Y FUNCIONAL 
- 
-# se crea un procedimiento para mostrar el titulo del proyecto
+"""
+
+PROYECTO SEMESTRAL ALGORITMOS 2025 - GESTOR DE NOTAS ACADÉMICAS
+
+Sistema para administrar cursos, calificaciones y calculos estadisticos basicos
+Implementa funcionalidades de búsqueda, ordenamiento y control de cambios
+
+"""
+
+# TITULO DEL PROYECTO
 def mostrarTitulo():
     print("* * * * * GESTOR DE NOTAS ACADÉMICAS * * * * *")
     print("MENÚ PRINCIPAL ")
 
 
-notas = [] # esto sera una lista que contenga cursos y sus respectivas notas y sera en una forma de tupla, es decir, parejas
-historial_pila = []
+# ESTRUCTURAS DE DATOS PRINCIPALES EN EL GESTOR DE NOTAS 
+notas = [] # lista que contenga las parejas (curso, nota)
+historial_pila = [] # pila (LIFO) para el resgistro de cambios realizador por el usuario
 
-# OPCION NO.1 
-# se crea una funcion que permita registrar un nuevo cursos
+
+# OPCION NO.1 - REGISTRAR NUEVO CURSO 
+# se registra un nuevo curso con su respectiva nota
 def registrar_nuevo_curso():
-    curso = input("INGRESE EL NOMBRE DEL CURSO QUE DESEA REGISTRAR: ")
+    curso = input("INGRESE EL NOMBRE DEL CURSO QUE DESEA REGISTRAR: ").strip().title()
     nota_str = input("INGRESE LA NOTA OBTENIDA PARA EL CURSO: ")
-    # se verifica que la nota sea numero o se convierta en numero y adicional que se encuentr en el rango de 0 a 100
+    # se verifica que sea un numero y se encuentre dentro del rango valido establecido
     while not nota_str.isdigit() or not (0 <= float(nota_str) <=100 ):
         print()
         print("NOTA FUERA DEL RANGO, POR FAVOR INTENTE CON UN VALOR ENTRE [0...100]")
@@ -28,10 +37,10 @@ def registrar_nuevo_curso():
     print("CURSO Y NOTA INGRESADOS CORRECTAMENTE")
     print()
      
-# OPCION NO.2
-# se crea una funcion que muestre todos los valores que el usuario ha ingresado (cursos y notas)
+     
+# OPCION NO.2 - MOSTRAR NOTAS Y CURSOS
+# muestra todos los cursos y notas registrados en el sistema
 def mostrar_cursosYnotas():
-    #se evalua que la lista tenga datos, de lo contrario, se le debe indicar al usuario
     if len(notas) == 0:
         print("NO EXISTEN CURSOS Y NOTAS INGRESADAS AL MOMENTO, POR FAVOR REGISTRE UNO NUEVO")
     else:
@@ -41,34 +50,32 @@ def mostrar_cursosYnotas():
             print(f"* CURSO: {curso} - NOTA: {nota}")
         print()
 
-# OPCION NO.3
-# se creara una funcion para el calculo del promedio
+
+# OPCION NO.3 - CALCULO DE PROMEDIO
+# calcula el promedio general con las notas ingresadas en el sistema
 def promedio_de_notas():
-    #verifiar que la lista no este vacia de lo contrario, se le indica al usuario
     if len(notas) == 0:
         print("NO HAY DATOS REGISTRADOS, POR FAVOR INGRESE UN CURSO Y UNA NOTA")
         print()
         return 
-    #usar un bucle for que vara recorriendo las notas dentro de la lista, las sume y cuente la cantidad
     suma_notas = 0
     cantidad_notas = 0
     for curso, nota in notas:
         suma_notas += nota 
         cantidad_notas += 1
-    #RETORNAR suma_notas / cantidad_notas
     promedio = suma_notas / cantidad_notas
     
     print()
     print(f"EL PROMEDIO ES DE: {promedio:.2f}")
     print()
+    
 
-# OPCION NO.4    
-#se crea una funcion que cuente los aprobados y los reprobados evaluando si la nota es >= 60
+# OPCION NO.4 - CONTAR APROBADOS Y REPROBADOS 
+# cuenta y muestra cuantos cursos ha aprobado el estudiante (>=60) y cuantos ha reprobado (<60)
 def contar_aprobadosYreprobados():
     aprobados = 0
     reprobados = 0
-    #el _, significa que no se tomara en cuenta el nombre del curso, sino solo la nota
-    for _, nota in notas:
+    for _, nota in notas: # el _, significa que no se tomara en cuenta el nombre del curso, sino solo la nota
         if nota >= 60:
             aprobados += 1
         else:
@@ -77,9 +84,10 @@ def contar_aprobadosYreprobados():
     print(f"EL TOTAL DE CURSOS APROBADOS ES DE: {aprobados}")
     print(f"EL TOTAL DE CURSOS REPROBADOS ES DE: {reprobados}")
     print()
+    
 
-# OPCION NO.5    
-# se crea una funcion que permita buscar un curso por medio del nombre
+# OPCION NO.5 - BUSCAR CURSO 
+# busca un curso dentro del sistema por nombre
 def buscar_curso():
     curso_buscado = input("INGRESE EL NOMBRE DEL CURSO QUE DESEA BUSCAR ")
     encontrado = False 
@@ -94,27 +102,22 @@ def buscar_curso():
         print()
         print("CURSO NO ENCONTRADO, POR FAVOR INTENTE NUEVAMENTE")
         print()
+        
 
-# OPCION NO.6        
-# se crea una funcino que permita actualizar una nota por el usuario
+# OPCION NO.6 - ACTUALIZAR NOTA DE CURSO
+# actualiza la nota de un curso ya existente y ese cambio lo guarda en la pila de historial de cambios
 def actualizar_nota():
-    curso_buscado = input("INGRESE EL NOMBRE DEL CURSO QUE DESEA ACTUALIZAR ")
-    #se verifica si el curso si se encontro
-    encontrado = False
+    curso_buscado = input("INGRESE EL NOMBRE DEL CURSO QUE DESEA ACTUALIZAR ").strip()
     
-    # la variable i va a recorrer el elemento de la lista notas
     for i in range(len(notas)):
-        #se compara el nombre de la posicion que tenga i para compararlo con el curso buscado
         if notas[i][0].lower() == curso_buscado.lower():
-            #si se encuentra el curso "encontrado" se vuelve True
-            
             nota_anterior = notas[i][1] # Captura la nota anterior
             
             nota_nueva = input("INGRESE LA NUEVA NOTA DEL CURSO ")
-            #se verifica que la nota nueva ingresada por el usuario este dentro del rango
-            while not nota_nueva.isdigit() or not (0 <= float(nota_nueva)):
+            while not nota_nueva.isdigit() or not (0 <= float(nota_nueva) <=100):
                 nota_nueva = input("NOTA FUERA DE RANGO, INTENTE NUEVAMENTE")
                 
+            # se registra el cambio en el historial    
             historial_pila.append(f"ACTUALIZADO: '{notas[i][0]}' de {nota_anterior} a {nota_nueva}")
                 
             # se crea una nueva lista con el nombre del curso y la nueva nota    
@@ -123,20 +126,22 @@ def actualizar_nota():
             print("NOTA ACTUALIZADA CON EXITO")
             print()
             return
-    if not encontrado:    
-        print("CURSO NO ENCONTRADO, POR FAVOR INTENTE NUEVAMENTE")
-        print()
-
-# OPCION NO.7        
-# # se crea una funcion que permita eliminar un curso de la lista segun la eleccion del usuario
-def eliminar_curso():
-    curso_a_eliminar = input("INGRESE EL NOMBRE DEL CURSO QUE DESEA ELIMINAR: ")
+        
+    print("CURSO NO ENCONTRADO, POR FAVOR INTENTE NUEVAMENTE")
     print()
-    permiso = input("¿DESEA ELIMINAR EL CURSO PERMANENTEMENTE SI O NO? ")
+
+
+# OPCION NO.7 - ELIMINAR CURSO
+# elimina un curso ya existente de la lista luego de una confirmacion por el usuario
+def eliminar_curso():
+    curso_a_eliminar = input("INGRESE EL NOMBRE DEL CURSO QUE DESEA ELIMINAR: ").strip()
+    print()
+    permiso = input("¿DESEA ELIMINAR EL CURSO PERMANENTEMENTE SI O NO? ").strip().upper()
     if permiso == "SI":
         for curso in notas:
             if curso[0].lower() == curso_a_eliminar.lower():
                 
+                # registra la eliminacion del curso dentro del historial
                 historial_pila.append(f"ELIMINADO: '{curso[0]}' con nota {curso[1]}")
                 
                 notas.remove(curso)
@@ -154,12 +159,11 @@ def eliminar_curso():
 print()
     
         
-# OPCION NO.8    
-# se crea una funcion que ordene los curos en base a su nota utilizando el algoritmo de ordnamiento de burbuja
+# OPCION NO.8 - ORDENAR POR NOTA    
+# ordena los cursos de la lista segun el valor de las notas, de menor a mayor utilizando algoritmo de ordenamiento de burbuja
 def ordenar_por_nota():
     n = len(notas)
     cambio = True
-    
     # el bucle while continua mientras se resten realizando intercambios
     while cambio:
         cambio = False
@@ -167,7 +171,6 @@ def ordenar_por_nota():
             if notas[i][1] > notas[i + 1][1]:
                 #se realiza el interbamcio entre notas
                 notas[i], notas[i + 1] = notas[i + 1], notas[i]
-                
                 cambio = True
         n -=1
     print()
@@ -176,8 +179,8 @@ def ordenar_por_nota():
     return notas
 
     
-# OPCION NO.9
-# se crea una funcion que ordena los cursos en base a su nombre utilizando el algoritmo de ordenamiento por insercion
+# OPCION NO.9 - ORDENAR POR NOMBRE
+# ordena los cursos de la lista en base a su nombre utilizando el algoritmo de ordenamiento por insercion
 def ordenar_por_nombre():
     n = len(notas)
     for i in range(1, n):
@@ -191,8 +194,9 @@ def ordenar_por_nombre():
     print("La lista ordenada por nombre: ", notas)
     print()
     
-# OPCION NO.10
-# se crea una funcion que simula una cola de solicitudes de revision ingresadas por el usuario
+    
+# OPCION NO.10 - COLA DE REVISION
+# esta funcion simula una cola (FIFO) de solicitudes de revision de cursos
 def cola_revision():
 
     cola_cursos = []
@@ -206,7 +210,7 @@ def cola_revision():
         
         elif opcion == "AGREGAR":
             # Pide el nombre del curso y lo agrega al final de la cola
-            curso_nuevo = input("INGRESE EL CURSO PARA REVISIÓN: ").strip()
+            curso_nuevo = input("INGRESE EL CURSO PARA REVISIÓN: ").strip().title()
             if curso_nuevo:
                 cola_cursos.append(curso_nuevo)
                 print("curso añadido a la cola de revision")
@@ -223,12 +227,16 @@ def cola_revision():
                 print("Se Revisó: ", curso_a_revisar)
             else:
                 print("No hay mas cursos para revisar")
+        else:
+            print("Opción Invalida. Use solamente 'AGREGAR', 'REVISAR' o 'FIN' ")
                 
         # Muestra el estado actual de la cola después de cada acción ingresada por el usuario
         print("La cola Actual de Revision es: ", cola_cursos)
+        print()
         
-# OPCION NO.11
-# se crea una funcion que permita visualizar los cambios recientes dentro del gestor de notas
+        
+# OPCION NO.11 - HISTORIAL DE CAMBIOS
+# muestra los cambios realizados por el usuario recientemente, se presenta de manera LIFO (mas reciente primero)
 def historial():
     if not historial_pila:
         print("NO HAY CAMBIOS REGISTRADOS ACTUALMENTE DENTRO DEL GESTOR DE NOTAS")
@@ -236,15 +244,15 @@ def historial():
 
     print()
     print("* * * * HISTORIAL DE CAMBIOS (MÁS RECIENTE PRIMERO) * * * *")
-    
+    print()
+
     # Se recorre la pila de atrás hacia adelante (LIFO)
     for i in range(1, len(historial_pila) + 1):
-        # Muestra el elemento -i, que corresponde a la posición LIFO
         cambio = historial_pila[-i] 
         print(f"  {i}. {cambio}")
+        print()
     
-
-                              
+ 
     
 # * * * * * * * * * CODIGO PRINCIPAL GESTOR DE NOTAS ACADEMICAS * * * * * * * *
 
@@ -267,7 +275,6 @@ while True:
     print("12. SALIR")
     opcion = input("POR FAVOR, SELECCIONE UNA OPCIÓN ")
     print()
-    
     
  # dependiendo de la opción seleccionada, se llamara a la funcion correspondiente   
     
